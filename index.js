fe8191e71dedeed4743bcb79b1b1c830df0bd36a
vinh-zalo <vinhtran19950804@gmail.com>
2017-12-08 19:05:22 +0700

    first push

---
+++ index.js
@@ -0,0 +1,27 @@
+const problem = {
+    start: {A: 5, B: 2},
+    A: {C:  4, D: 2},
+    B: {A: 8, D: 7},
+    C: {D: 6, finish: 3},
+    finish: {}
+};
+
+const lowestCostNode = (costs, processed) => {
+    return Object.keys(costs).reduce((lowest, node) => {
+        if (lowest === null || costs[node] < costs[lowest]) {
+            if(!processed.includes(node)) {
+                lowest = node;
+            }
+        }
+        return lowest;
+    }, null)
+}
+
+const dijkstra = (graph) => {
+    const cost = Object.assign({finish: Infinity}, graph.start);
+
+    const parents = {finish: null};
+    for (let child in graph.start) {
+        parents[child] = 'start';
+    }
+}

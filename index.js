70867f259670069c3ab28009f7d5d2a14334e3c8
vinh-zalo <vinhtran19950804@gmail.com>
2017-12-11 10:21:17 +0700

    update newest

--- index.js
+++ index.js
@@ -22,6 +22,28 @@ const dijkstra = (graph) => {
 
     const parents = {finish: null};
     for (let child in graph.start) {
-        parents[child] = 'start';
+        parents[child] = 'start'; 
+    }
+
+    const processed = [];
+
+    let node = lowestCostNode(cost, processed);
+
+    while (node) {
+        let cost = costs[node];
+        let children = graph[node];
+        for (let n in children) {
+            let newCost = cost + children[n];
+            if(!costs[n]) {
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+            if(costs[n] > newCost) {
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+        }
+        processed.push(node);
+        node = lowestCostNode(costs, processed);
     }
 }
